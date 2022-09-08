e9a86802a23a0c8bea86ff9bed6094781f71daf9
rumeone <yuaroslav_29@mail.ru>
2022-09-08 23:54:53 +0700

    Added post model

--- src/models/models.js
+++ src/models/models.js
@@ -4,7 +4,16 @@ const sequelize = require('../db');
 const User = sequelize.define('user', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     email: {type: DataTypes.STRING, unique: true},
-    password: {type: DataTypes.STRING}
+    password: {type: DataTypes.STRING},
 });
 
-module.exports = {User};
+const Post = sequelize.define('post', {
+    author: {type: DataTypes.STRING},
+    title: {type: DataTypes.STRING},
+    content: {type: DataTypes.STRING}
+});
+
+User.hasMany(Post);
+Post.belongsTo(User);
+
+module.exports = {User, Post};
